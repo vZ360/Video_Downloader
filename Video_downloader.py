586308fa34d66d1336a3035418aa34e9177d10d5
Paulson Baby <babypaulsonbaby143@gmail.com>
2021-07-21 10:47:40 +0530

    Update Video_downloader.py
    Edited a comment

--- Video_downloader.py
+++ Video_downloader.py
@@ -1,6 +1,6 @@
 import pytube
 link = input("Enter Youtube Link Here >> ") #enter link here
-SAVE_PATH = "D:\"                          #file name 
+SAVE_PATH = "D:\"                          #file path comment edited by paulson
 yt = pytube.YouTube(link)
 stream = yt.streams.first()
 stream.download(SAVE_PATH) 
